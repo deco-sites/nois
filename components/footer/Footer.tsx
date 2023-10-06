967058d515d73b3af7407458b3f9321735c5b4f2
Diego <diego.o.guimaraes29@gmail.com>
2023-10-06 15:36:21 -0300

    feat: create footer

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,216 @@
+import Icon, { AvailableIcons } from "$store/components/ui/Icon.tsx";
+import type { ComponentChildren } from "preact";
+import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
+import Image from "deco-sites/std/components/Image.tsx";
+
+export type IconItem = { icon: AvailableIcons; href: string };
+export type StringItem = {
+  label: string;
+  href?: string;
+  /** @default blank */
+  target?: "blank" | "self";
+};
+export type StringNewsletter = {
+  Title: string;
+  desc?: string;
+  successText: string;
+  errorText: string;
+};
+
+export type Item = StringItem | IconItem;
+
+export type Section = {
+  label: string;
+  children: Item[];
+};
+
+const isIcon = (item: Item): item is IconItem =>
+  // deno-lint-ignore no-explicit-any
+  typeof (item as any)?.icon === "string";
+
+function SectionItem({ item }: { item: Item }) {
+  return (
+    <span class="text-white text-[14px]">
+      {isIcon(item)
+        ? (
+          <div class="border-white border border-solid py-1.5 px-2.5">
+            <Icon
+              id={item.icon}
+              width={25}
+              height={20}
+              strokeWidth={0.01}
+            />
+          </div>
+        )
+        : (
+          item.href
+            ? (
+              <a
+                href={item.href}
+                target={item.target ? `_${item.target}` : "_blank"}
+              >
+                {item.label}
+              </a>
+            )
+            : <span>{item.label}</span>
+        )}
+    </span>
+  );
+}
+
+function FooterContainer(
+  { children, class: _class = "" }: {
+    class?: string;
+    children: ComponentChildren;
+  },
+) {
+  return <div class={`py-6 px-4 sm:py-12 sm:px-0 ${_class}`}>{children}</div>;
+}
+
+export interface Props {
+  Service: string;
+  Phone_Number: string;
+  Href_Instagram: string;
+  Href_Facebook: string;
+  Href_Twitter: string;
+  sections?: Section[];
+  titleNewsletter: string;
+  descNewsletter: string;
+  successTextNewsletter: string;
+  errorTextNewsletter: string;
+  imgFooter: LiveImage;
+}
+
+function Footer(
+  {
+    sections = [],
+    Service,
+    Phone_Number,
+    Href_Instagram,
+    Href_Facebook,
+    Href_Twitter,
+    titleNewsletter,
+    descNewsletter,
+    successTextNewsletter,
+    errorTextNewsletter,
+    imgFooter,
+  }: Props,
+) {
+  return (
+    <>
+      <footer class="before:content-[''] before:absolute before:top-0 before:left-0 before:bottom-0 before:right-0 before:z-[9] before:opacity-20 before:bg-cover
+      before:bg-[url('https://bawclothing.vtexassets.com/assets/vtex/assets-builder/bawclothing.theme/4.5.13/img/bg-img-footer___f97b5671fcf089ebf7415649360e7e9a.png')] 
+      relative w-full sm:bg-primary-dark flex flex-col divide-y divide-primary-content sm:pb-[80px] sm:px-8 bg-[#000]">
+        <div class=" w-full flex flex-col divide-y divide-primary-content border-b border-[#9a9a9a79] z-10">
+          <FooterContainer>
+            <div class="flex sm:hidden mb-4">
+           
+            </div>
+            {/* Desktop view */}
+            <ul class="flex flex-col lg:flex-row justify-start gap-8 lg:justify-between">
+              {sections.map((section) => (
+                <li>
+                  <span class="text-[14px] text-[#9a9a9a]">
+                    {section.label}
+                  </span>
+
+                  <ul
+                    class={`flex ${
+                      isIcon(section.children[0]) ? "flex-row" : "flex-col"
+                    } gap-2 pt-2 flex-wrap`}
+                  >
+                    {section.children.map((item) => (
+                      <li>
+                        <SectionItem item={item} />
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+
+              <li>
+                <span class="text-[14px] text-[#9a9a9a]">
+                  Fale Conosco
+                </span>
+                <ul class="flex flex-col gap-2 pt-2 flex-wrap">
+                  <li>
+                    <SectionItem item={{ label: "SAC " + Phone_Number }} />
+                  </li>
+                  <li>
+                    <SectionItem
+                      item={{ label: "Horários de Atendimento" }}
+                    />
+                  </li>
+                  <li>
+                    <SectionItem item={{ label: Service }} />
+                  </li>
+                  <li>
+                    <div class="flex flex-row object-center justify-start text-end gap-5">
+                  
+
+                     
+                      <a
+                        href={Href_Instagram}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label="Instagram logo"
+                      >
+                        <Icon
+                          class="text-white pb-1"
+                          width={32}
+                          height={32}
+                          id="Instagram"
+                          strokeWidth={2}
+                        />
+                      </a>
+                    </div>
+                  </li>
+                </ul>
+              </li>
+            
+            </ul>
+          </FooterContainer>
+        </div>
+      </footer>
+
+      <div class="w-full bg-white">
+        <div class="container w-full">
+          <FooterContainer class="flex justify-between w-full">
+            <div class="">
+              <div>
+                <span class="text-xs">
+                  © Baw Clothing - Todos os direitos reservado - BAW Clothing
+                  Indústria e Comércio de Vestuários Ltda. CNPJ
+                  27.238.862/0001-03 SAC (11) 3181-6118
+                </span>
+              </div>
+              <div class="mt-2">
+                <span class="flex items-center gap-1 text-[#747474] text-xs">
+                  Powered by{" "}
+                  <a
+                    href="https://www.deco.cx"
+                    aria-label="powered by https://www.deco.cx"
+                  >
+                    <Icon id="Deco" height={15} width={45} strokeWidth={0.01} />
+                  </a>
+                </span>
+              </div>
+            </div>
+
+            <div class="flex">
+              <Image
+                height={200}
+                width={200}
+                loading={"lazy"}
+                src={"https://bawclothing.vtexassets.com/assets/vtex.file-manager-graphql/images/cb2b8e5a-8783-4090-a3b9-aecc33ce8c42___206c3b8b0da92d76e40c362f80f2cf6a.svg"}
+                alt="credit cards"
+              />
+            </div>
+          </FooterContainer>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
