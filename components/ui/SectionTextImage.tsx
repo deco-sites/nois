1878b6f613e93d7d6b91409e67d61d7de96e5ab7
Diego <diego.o.guimaraes29@gmail.com>
2023-10-09 20:03:36 -0300

    lint

--- components/ui/SectionTextImage.tsx
+++ components/ui/SectionTextImage.tsx
@@ -3,22 +3,20 @@ import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
 import Image from "deco-sites/std/components/Image.tsx";
 
 export interface Props {
-  
   title?: string;
   titleHighlight?: string;
   text?: string;
   image?: LiveImage;
   altImage?: string;
-  texts? :Text[];
+  texts?: Text[];
 }
 export type Text = {
   text: string;
-  size?: 48 | 36 | 20  |18 ;
+  size?: 48 | 36 | 20 | 18;
   gradient?: boolean;
-  bold: "semibold"| "bold" | "none"
-  italic: boolean
-  color: "black" | "white" | "green"
-
+  bold: "semibold" | "bold" | "none";
+  italic: boolean;
+  color: "black" | "white" | "green";
 };
 export default function BannnerGrid({
   title,
@@ -26,27 +24,19 @@ export default function BannnerGrid({
   image,
   altImage,
   text,
-  texts
+  texts,
 }: Props) {
   return (
     <div class="w-full bg-[#1E2D32] px-[80px] py-[40px]">
       <section class="flex flex-row justify-around items-center w-full mx-auto min-h-[640px]">
         <div class=" flex flex-col justify-center items-start gap-2 max-w-[690px] ">
-       
-         
-              <div class="flex  flex-col text-left text-5xl mb-[40px]">
-                            
-                  <span class=" uppercase text-transparent bg-clip-text bg-gradient-to-t from-[#9990FF]  to-[#76DBD3] ">
-                  
-                  
-                  <span class="font-medium">{title}</span> 
-                  <span class="font-extrabold">  {titleHighlight}</span> 
+          <div class="flex  flex-col text-left text-5xl mb-[40px]">
+            <span class=" uppercase text-transparent bg-clip-text bg-gradient-to-t from-[#9990FF]  to-[#76DBD3] ">
+              <span class="font-medium">{title}</span>
+              <span class="font-extrabold">{titleHighlight}</span>
+            </span>
+          </div>
 
-                 
-                  </span>
-              
-              </div>
-         
           {text &&
             (
               <div class=" flex text-left w-[690px]">
@@ -61,15 +51,14 @@ export default function BannnerGrid({
         >
           {image &&
             (
-           
-                <Image
-                  class="w-full object-cover"
-                  src={image}
-                  alt={altImage}
-                  width={830}
-                  height={700}
-                  loading="lazy"
-                />
+              <Image
+                class="w-full object-cover"
+                src={image}
+                alt={altImage}
+                width={830}
+                height={700}
+                loading="lazy"
+              />
             )}
         </div>
       </section>
